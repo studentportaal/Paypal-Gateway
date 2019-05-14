86c3a2cc323272c89ea99959764b4693c1714e13
daniel5671 <daniellin1995@hotmail.com>
2019-05-14 12:26:07 +0200

    braintree setup

---
+++ Paypal-Gateway/Controllers/PaymentController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Braintree;
+using Microsoft.AspNetCore.Mvc;
+using PaypalGateway.Domain;
+using PaypalGateway.Interfaces;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PaypaGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : Controller
+    {
+        public IBraintreeConfiguration Config = new BrainTreeConfiguration();
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("token/new")]
+        public object CreateToken ()
+        {
+            var gateway = Config.GetGateway();
+            var clienttoken = gateway.ClientToken.Generate();
+            return clienttoken;
+
+        }
+
+        [HttpPost]
+        [Route("transaction")]
+        public object CheckOut(Company company)
+        {
+            string paymentstatus = string.Empty;
+            var gateway = Config.GetGateway();
+            var request = new TransactionRequest
+            {
+                Amount = company.Price,
+                PaymentMethodNonce = company.PaymentMethodNonce,
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            Result<Transaction> transactionResult = gateway.Transaction.Sale(request);
+
+            if (transactionResult.IsSuccess())
+            {
+                paymentstatus = "succeeded";
+            }
+            else
+            {
+                string errormessages = "";
+                foreach (ValidationError error in transactionResult.Errors.DeepAll())
+                {
+                    errormessages += "Error: " + error.Message;
+                }
+                paymentstatus = errormessages;
+            }
+            return paymentstatus;
+        }
+    }
+
+}
---
+++ Paypal-Gateway/Domain/BrainTreeConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Braintree;
+using PaypalGateway.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaypalGateway.Domain
+{
+    public class BrainTreeConfiguration : IBraintreeConfiguration
+    {
+        public string Environment { get; set; }
+        public string MerchantId { get; set; }
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
+        private IBraintreeGateway BraintreeGateway { get; set; }
+
+
+        public IBraintreeGateway CreateGateway()
+        {
+            // TODO: put these values in environmentVariables
+            Environment = System.Environment.GetEnvironmentVariable("BrainTreeEnvironment");
+            MerchantId = System.Environment.GetEnvironmentVariable("BraintreeMerchantId");
+            PublicKey = System.Environment.GetEnvironmentVariable("BraintreePublicKey");
+            PrivateKey = System.Environment.GetEnvironmentVariable("BrainTreePrivateKey");
+
+            return new BraintreeGateway(Environment, MerchantId, PublicKey, PrivateKey);
+        }
+
+        public IBraintreeGateway GetGateway()
+        {
+            if (BraintreeGateway == null)
+            {
+                BraintreeGateway = CreateGateway();
+            }
+
+            return BraintreeGateway;
+        }
+    }
+}
